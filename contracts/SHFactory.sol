cd86de1a82b7a44de6be2cf25522557b36e81f44
Paul <phongcpt72@gmail.com>
2024-11-14 11:22:11 +0700

    remove setProductName function

--- contracts/SHFactory.sol
+++ contracts/SHFactory.sol
@@ -31,11 +31,6 @@ contract SHFactory is ISHFactory, OwnableUpgradeable {
        uint256 maxCapacity
    );
 
-   event ProductUpdated(
-       address indexed product,
-       string name
-   );
-
    /**
     * @dev Initializes the contract setting the deployer as the initial owner.
     */
@@ -97,20 +92,6 @@ contract SHFactory is ISHFactory, OwnableUpgradeable {
        products.push(productAddr);
 
        emit ProductCreated(productAddr, _name, _underlying, _maxCapacity);
-   }
-
-   /**
-    * @notice set new product name
-    */
-   function setProductName(string memory _name, address _product) external onlyOwner {
-       require(getProduct[_name] == address(0), "Product already exists");
-       require(isProduct[_product], "Invalid product address");
-       string memory _oldName = ISHProduct(_product).name();
-       delete getProduct[_oldName];
-       getProduct[_name] = _product;
-       ISHProduct(_product).updateName(_name);
-
-       emit ProductUpdated(_product, _name);
    }
 
    /**
