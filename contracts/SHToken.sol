358750a16a23ff1804b5c67500772837654c7fc8
Paul <phongcpt72@gmail.com>
2024-11-26 22:52:55 +0700

    fix 7.20

--- contracts/SHToken.sol
+++ contracts/SHToken.sol
@@ -4,10 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract SHToken is ERC20, AccessControl {
-    using SafeMath for uint256;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
