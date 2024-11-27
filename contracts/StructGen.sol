8e5d9072897f273afab42b8c03a2432f76e2a789
Paul <phongcpt72@gmail.com>
2024-11-27 08:59:23 +0700

    add fixing 7.29

--- contracts/StructGen.sol
+++ contracts/StructGen.sol
@@ -5,6 +5,13 @@ import "@pendle/core-v2/contracts/interfaces/IPAllActionV3.sol";
 import "@pendle/core-v2/contracts/interfaces/IPMarket.sol";
 
 abstract contract StructGen {
+
+    uint256 public guessMin = 0; // adjust as desired
+    uint256 public guessMax = type(uint256).max; // adjust as desired
+    uint256 public guessOffchain = 0; // strictly 0
+    uint256 public maxIteration = 256; // adjust as desired
+    uint256 public eps = 1e14; // max 0.01% unused, adjust as desired
+
     // EmptySwap means no swap aggregator is involved
     SwapData public emptySwap;
 
@@ -12,7 +19,7 @@ abstract contract StructGen {
     LimitOrderData public emptyLimit;
 
     // DefaultApprox means no off-chain preparation is involved, more gas consuming (~ 180k gas)
-    ApproxParams public defaultApprox = ApproxParams(0, type(uint256).max, 0, 256, 1e14);
+    ApproxParams public defaultApprox = ApproxParams(guessMin, guessMax, guessOffchain, maxIteration, eps);
 
     /// @notice create a simple TokenInput struct without using any aggregators. For more info please refer to
     /// IPAllActionTypeV3.sol
