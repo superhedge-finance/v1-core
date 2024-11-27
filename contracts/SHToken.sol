127a22f1d3b63ff443e7af2212883dce0c948e6e
Paul <phongcpt72@gmail.com>
2024-11-27 08:42:29 +0700

    fix more 7.28

--- contracts/SHToken.sol
+++ contracts/SHToken.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract SHToken is ERC20, AccessControl {
