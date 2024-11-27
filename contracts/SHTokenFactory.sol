6297706a1b50c21865ee583d726996ce305be74b
Paul <phongcpt72@gmail.com>
2024-11-27 12:35:47 +0700

    update 7.14

--- contracts/SHTokenFactory.sol
+++ contracts/SHTokenFactory.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
+pragma solidity ^0.8.9;
+import "./interfaces/ISHTokenFactory.sol";
 import "./SHToken.sol";
 
-contract SHTokenFactory {
+contract SHTokenFactory is ISHTokenFactory {
     event TokenCreated(address indexed tokenAddress, string name, string symbol, address creator);
 
     function createToken(string memory name, string memory symbol, address _owner) external returns (address) {
